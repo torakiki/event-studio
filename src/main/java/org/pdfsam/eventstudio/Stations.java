f26b566ae466b7a8feb7ca7406730fe359aaa5a3
torakiki <andrea.vacondio@gmail.com>
2026-07-08 08:49:58 +0200

    Cleaned code a bit

--- src/main/java/org/pdfsam/eventstudio/Stations.java
+++ src/main/java/org/pdfsam/eventstudio/Stations.java
@@ -1,36 +1,36 @@
-/* 
+/*
  * This file is part of the EventStudio source code
  * Created on 10/nov/2013
  *  Copyright 2020 by Sober Lemur S.r.l. (info@pdfsam.org).
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- * See the License for the specific language governing permissions and 
- * limitations under the License. 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.pdfsam.eventstudio;
 
-import static org.pdfsam.eventstudio.util.RequireUtils.requireNotBlank;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.pdfsam.eventstudio.util.RequireUtils.requireNotBlank;
 
 /**
  * A thread safe holder for {@link Station}. Provides methods to access to the {@link Station}s of the {@link EventStudio} creating a new one when required.
- * 
+ *
  * @author Andrea Vacondio
- * 
+ *
  */
 class Stations {
 
@@ -40,23 +40,17 @@ class Stations {
 
     /**
      * @return the station with the given name. It safely creates a new {@link Station} if a station with the given name does not exist.
-     * @throws IllegalArgumentException
-     *             if the station name is blank or null
+     * @throws IllegalArgumentException if the station name is blank or null
      */
     Station getStation(String stationName) {
         requireNotBlank(stationName);
-        Station station = stations.get(stationName);
-        if (station == null) {
-            final Station value = new Station(stationName);
-            station = stations.putIfAbsent(stationName, value);
-            if (station == null) {
-                station = value;
-                LOG.debug("Created station {}", station);
-            }
-        }
-        return station;
+        return stations.computeIfAbsent(stationName, name -> {
+            var station = new Station(name);
+            LOG.debug("Created station {}", station);
+            return station;
+        });
     }
-    
+
     /**
      * @return the collection of the configured stations
      * @see ConcurrentHashMap#values()
@@ -65,8 +59,8 @@ class Stations {
         return stations.values();
     }
 
-    void clear(String station) {
-        LOG.debug("Clearing station {}", station);
-        stations.remove(station);
+    void clear(String stationName) {
+        LOG.debug("Clearing station {}", stationName);
+        stations.remove(stationName);
     }
 }
